8a0f56266b16de0d0adadee5e40c8fc5aeeb8b91
William E. Sollers III <wsollers@gmail.com>
2025-01-13 10:25:39 -0500

    Add tests

--- tests/httptests.cpp
+++ tests/httptests.cpp
@@ -11,3 +11,24 @@ TEST(HttpProtocolTest, MatchesExpectedValues) {
     EXPECT_EQ(Http::getProtocolString(Http::Protocol::HTTP2), "HTTP/2");
     EXPECT_EQ(Http::getProtocolString(Http::Protocol::HTTP3), "HTTP/3");
 }
+
+// Test case for Transport enum class
+TEST(HttpTransportTest, MatchesExpectedValues) {
+    EXPECT_EQ(Http::getTransportString(Http::Transport::TLS), "HTTPS");
+    EXPECT_EQ(Http::getTransportString(Http::Transport::HTTP), "HTTP");
+
+}
+
+// Unit Test for getMethodString
+TEST(GetMethodStringTest, ReturnsExpectedStrings) {
+    // Test all enum values
+    EXPECT_EQ(Http::getMethodString(Http::Method::GET), "GET");
+    EXPECT_EQ(Http::getMethodString(Http::Method::POST), "POST");
+    EXPECT_EQ(Http::getMethodString(Http::Method::PUT), "PUT");
+    EXPECT_EQ(Http::getMethodString(Http::Method::DELETE_), "DELETE");
+    EXPECT_EQ(Http::getMethodString(Http::Method::HEAD), "HEAD");
+    EXPECT_EQ(Http::getMethodString(Http::Method::OPTIONS), "OPTIONS");
+    EXPECT_EQ(Http::getMethodString(Http::Method::PATCH), "PATCH");
+    EXPECT_EQ(Http::getMethodString(Http::Method::TRACE), "TRACE");
+    EXPECT_EQ(Http::getMethodString(Http::Method::CONNECT), "CONNECT");
+}
